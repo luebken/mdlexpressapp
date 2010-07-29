3ee676c8f58d920ea2a3b31152a91fb96828e0d9
Matthias Lübken <matthias.luebken@gmail.com>
2010-07-29 22:25:03 +0200

    typo

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ var http = require('http'),
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'})
   res.end('Hello World\n')
-}).listen(parseInt(port)
+}).listen(parseInt(port))
