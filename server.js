082d411a37b4ed91c7c2ea52eb29862ee12b4cba
Matthias Lübken <matthias.luebken@gmail.com>
2010-07-30 10:01:04 +0200

    cleanup

--- server.js
+++ server.js
@@ -8,13 +8,3 @@ require("sass")
 
 //require the actual express app
 require ("./lib/app")
-
-/*
-var http = require('http'),
-    port = process.env.PORT || 8001
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('Hello World 4\n')
-}).listen(parseInt(port))
-*/
