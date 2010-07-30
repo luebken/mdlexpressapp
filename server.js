e8ffd3765a32d22bace447dc216d818b17eb28c7
Matthias Lübken <matthias.luebken@gmail.com>
2010-07-30 09:51:01 +0200

    inserted require to connect

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+require.paths.unshift("vendor/lib")
+
+// require express and others to make sure
+//require("express")
+require("connect")
+//require("jade")
+//require("sass")
+
+//require the actual express app
+require ("./lib/app")
+
 var http = require('http'),
     port = process.env.PORT || 8001
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('Hello World 1\n')
+  res.end('Hello World 2\n')
 }).listen(parseInt(port))
